00ff1f35abb40336850992a4d1f41f624b317f70
dam2452 <81230036+dam2452@users.noreply.github.com>
2023-10-17 14:27:22 +0200

    New File

---
+++ Source/EndedWorld/Public/EWPlayerState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "EWPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ENDEDWORLD_API AEWPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+};
