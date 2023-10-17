f531b9da280f7956b6150409ba563bd0f0301555
dam2452 <81230036+dam2452@users.noreply.github.com>
2023-10-17 14:07:08 +0200

    Default Zoom fix

--- Source/EndedWorld/Private/EW_Character.cpp
+++ Source/EndedWorld/Private/EW_Character.cpp
@@ -1,27 +1,37 @@
 //EW_Character.cpp
-
-
 #include "EW_Character.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
-#include <GameFramework/CharacterMovementComponent.h>
+#include "GameFramework/CharacterMovementComponent.h"
 #include "Net/UnrealNetwork.h"
 #include "Components/WidgetComponent.h"
 
-
 AEW_Character::AEW_Character()
+	: DesiredLocation(FVector::ZeroVector),
+	Movement_Interp(5.0f),
+	Movement_Speed(30.0f),
+	DesiredYawRotation(0.0f),
+	Rotation_Interp(10.0f),
+	Rotation_Speed(5.0f),
+	Zoom_Interp(5.0f),
+	ZoomSpeed(70.0f),
+	MinZoomDistance(500.0f),
+	MaxZoomDistance(2800.0f)
 {
- 	
 	PrimaryActorTick.bCanEverTick = true;
 
+	InitializeComponents();
+}
+
+void AEW_Character::InitializeComponents()
+{
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(GetMesh());
 	CameraBoom->TargetArmLength = 1800.0f;
 	CameraBoom->bUsePawnControlRotation = false;
 	CameraBoom->bDoCollisionTest = false;
-	//CameraBoom->SocketOffset = FVector(0.f, 0.f, 300.f);
 	CameraBoom->SetRelativeRotation(FRotator(-65.f, 0.f, 0.f));
 
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
@@ -29,63 +39,37 @@ AEW_Character::AEW_Character()
 	FollowCamera->bUsePawnControlRotation = false;
 
 	bUseControllerRotationYaw = false;
-	//bOrientRotationToMovement = false;
 	GetCharacterMovement()->SetIsReplicated(true);
-
-	
-	DesiredLocation = FVector::ZeroVector;
-	Movement_Interp = 5.0f;
-	Movement_Speed = 30.0f;
-
-	
-	DesiredYawRotation = 0.0f;
-	Rotation_Interp = 10.0f;
-	Rotation_Speed = 5.0f;
-
-	DesiredZoom = CameraBoom->TargetArmLength;
-	Zoom_Interp = 5.0f;
-	ZoomSpeed = 70.0f;
-	MinZoomDistance = 500.0f;
-	MaxZoomDistance = 2800.0f;
-
-	bReplicates = true;
-	//bReplicateMovement = true;
 
 	OverheadWidget = CreateDefaultSubobject<UWidgetComponent>(TEXT("OverheadWidget"));
 	OverheadWidget->SetupAttachment(RootComponent);
+
+	DesiredZoom = CameraBoom->TargetArmLength;
 }
 
 void AEW_Character::BeginPlay()
 {
 	Super::BeginPlay();
 
-
-	
 	if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
 	{
 		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
 		{
-
-			Subsystem->AddMappingContext(InputMappingContext,0);
-
+			Subsystem->AddMappingContext(InputMappingContext, 0);
 		}
 	}
-
-
 }
 
 void AEW_Character::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
-	
 	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
 	{
 		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AEW_Character::Move);
 		EnhancedInputComponent->BindAction(RotateAction, ETriggerEvent::Triggered, this, &AEW_Character::Rotate);
 		EnhancedInputComponent->BindAction(ZoomAction, ETriggerEvent::Triggered, this, &AEW_Character::Zoom);
 	}
-
 }
 
 void AEW_Character::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
@@ -105,83 +89,76 @@ void AEW_Character::Multicast_SetDesiredLocation_Implementation(const FVector& N
 void AEW_Character::Move(const FInputActionValue& Value)
 {
 	FVector2D MoveInput2D = Value.Get<FVector2D>();
-	FVector MoveInput(MoveInput2D.X, MoveInput2D.Y, 0.f); // Convert FVector2D to FVector
+	FVector MoveInput(MoveInput2D.X, MoveInput2D.Y, 0.f);
 
-	HandleInput(MoveInput, 0.f, 0.f);
-	if (!HasAuthority())
-	{
-		Server_HandleInput(MoveInput, 0.f, 0.f);
-	}
+	ProcessInput(MoveInput, 0.f, 0.f);
 }
-
-
 
 void AEW_Character::Rotate(const FInputActionValue& Value)
 {
 	float RotateInput = Value.Get<float>();
-	HandleInput(FVector::ZeroVector, RotateInput, 0.f);
-	if (!HasAuthority())
-	{
-		Server_HandleInput(FVector::ZeroVector, RotateInput, 0.f);
-	}
+	ProcessInput(FVector::ZeroVector, RotateInput, 0.f);
 }
+
 void AEW_Character::Zoom(const FInputActionValue& Value)
 {
 	float ZoomInput = Value.Get<float>();
-	HandleInput(FVector::ZeroVector, 0.f, ZoomInput);
-	if (!HasAuthority())
-	{
-		Server_HandleInput(FVector::ZeroVector, 0.f, ZoomInput);
-	}
+	ProcessInput(FVector::ZeroVector, 0.f, ZoomInput);
 }
 
+void AEW_Character::ProcessInput(const FVector& MoveInput, const float& RotateInput, const float& ZoomInput)
+{
+	HandleInput(MoveInput, RotateInput, ZoomInput);
+	if (!HasAuthority())
+	{
+		Server_HandleInput(MoveInput, RotateInput, ZoomInput);
+	}
+}
 
 void AEW_Character::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	// Move for server and client
+	UpdateMovement(DeltaTime);
+	UpdateRotation(DeltaTime);
+	UpdateZoom(DeltaTime);
+}
+
+void AEW_Character::UpdateMovement(float DeltaTime)
+{
 	FVector CurrentLocation = GetActorLocation();
 	FVector NewLocation = FMath::VInterpTo(CurrentLocation, DesiredLocation, DeltaTime, Movement_Interp);
 	SetActorLocation(NewLocation);
+}
 
-	// Rotation
+void AEW_Character::UpdateRotation(float DeltaTime)
+{
 	FRotator CurrentRotation = GetActorRotation();
 	FRotator NewRotation = FMath::RInterpTo(GetActorRotation(), FRotator(0, DesiredYawRotation, 0), DeltaTime, Rotation_Interp);
 	SetActorRotation(NewRotation);
+}
 
-	// Zoom
+void AEW_Character::UpdateZoom(float DeltaTime)
+{
 	float CurrentZoom = CameraBoom->TargetArmLength;
 	float NewZoom = FMath::FInterpTo(CurrentZoom, DesiredZoom, DeltaTime, Zoom_Interp);
 	CameraBoom->TargetArmLength = NewZoom;
 }
 
-
-
 void AEW_Character::HandleInput(const FVector& MoveInput, const float& RotateInput, const float& ZoomInput)
 {
-	// Movement logic
 	const FRotator YawRotation(0.f, FollowCamera->GetComponentRotation().Yaw, 0.f);
 	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
 	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
 
-	DesiredLocation += (ForwardDirection * MoveInput.X + RightDirection * MoveInput.Y + ForwardDirection * MoveInput.Z) * Movement_Speed;
-
+	DesiredLocation += (ForwardDirection * MoveInput.X + RightDirection * MoveInput.Y) * Movement_Speed;
 	if (HasAuthority())
 	{
 		Multicast_SetDesiredLocation(DesiredLocation);
 	}
-	else
-	{
-		// This assumes that your Server_Move function or similar can handle FVector input for movement
-		// Server_Move(FInputActionValue(MoveInput));
-	}
 
-	// Rotation logic
 	DesiredYawRotation += RotateInput * Rotation_Speed;
-	SetActorRotation(FRotator(0, DesiredYawRotation, 0));
 
-	// Zoom logic
 	DesiredZoom = FMath::Clamp(CameraBoom->TargetArmLength + ZoomInput * ZoomSpeed, MinZoomDistance, MaxZoomDistance);
 }
 
@@ -189,6 +166,7 @@ void AEW_Character::Server_HandleInput_Implementation(FVector MoveInput, float R
 {
 	HandleInput(MoveInput, RotateInput, ZoomInput);
 }
+
 bool AEW_Character::Server_HandleInput_Validate(FVector MoveInput, float RotateInput, float ZoomInput)
 {
 	return true;
--- Source/EndedWorld/Public/EW_Character.h
+++ Source/EndedWorld/Public/EW_Character.h
@@ -1,13 +1,11 @@
 //EW_Character.h
-
 #pragma once
 
-#include "InputMappingContext.h"
-#include "InputAction.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "InputMappingContext.h"
+#include "InputAction.h"
 #include "EW_Character.generated.h"
-
 
 UCLASS()
 class ENDEDWORLD_API AEW_Character : public ACharacter
@@ -16,86 +14,106 @@ class ENDEDWORLD_API AEW_Character : public ACharacter
 
 public:
 	AEW_Character();
-	virtual void Tick(float DeltaTime) override;
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-	virtual void GetLifetimeReplicatedProps(TArray< FLifetimeProperty >& OutLifetimeProps) const override;
-
-	UFUNCTION(NetMulticast, Reliable)
-	void Multicast_SetDesiredLocation(const FVector& NewDesiredLocation);
-	virtual void Multicast_SetDesiredLocation_Implementation(const FVector& NewDesiredLocation);
-
-	UFUNCTION(Server, Reliable, WithValidation)
-	void Server_HandleInput(FVector MoveInput, float RotateInput, float ZoomInput);
-	virtual bool Server_HandleInput_Validate(FVector MoveInput, float RotateInput, float ZoomInput);
-	virtual void Server_HandleInput_Implementation(FVector MoveInput, float RotateInput, float ZoomInput);
 
 protected:
 	virtual void BeginPlay() override;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	// Initialize character components
+	void InitializeComponents();
+
+	virtual void Tick(float DeltaTime) override;
+
+	// Update character movement, rotation, and zoom
+	void UpdateMovement(float DeltaTime);
+	void UpdateRotation(float DeltaTime);
+	void UpdateZoom(float DeltaTime);
+
+	// Setup the player input components
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// Replicated properties
+	virtual void GetLifetimeReplicatedProps(TArray< FLifetimeProperty >& OutLifetimeProps) const override;
+
+	// Handle input
+	void HandleInput(const FVector& MoveInput, const float& RotateInput, const float& ZoomInput);
+
+	// Input Actions
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
 	UInputMappingContext* InputMappingContext;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
 	UInputAction* MoveAction;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
 	UInputAction* RotateAction;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
 	UInputAction* ZoomAction;
 
+	// Camera Components
+	UPROPERTY(VisibleAnywhere, Category = "Camera")
+	class USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, Category = "Camera")
+	class UCameraComponent* FollowCamera;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"), Category = "UI")
+	class UWidgetComponent* OverheadWidget;
+
+	// Movement Variables
+	UPROPERTY(Replicated, EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	FVector DesiredLocation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	float Movement_Interp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	float Movement_Speed;
+
+	// Rotation Variables
+	UPROPERTY(Replicated, EditAnywhere, BlueprintReadWrite, Category = "Rotation")
+	float DesiredYawRotation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Rotation")
+	float Rotation_Interp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Rotation")
+	float Rotation_Speed;
+
+	// Zoom Variables
+	UPROPERTY(Replicated, EditAnywhere, BlueprintReadWrite, Category = "Zoom")
+	float DesiredZoom;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Zoom")
+	float ZoomSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Zoom")
+	float Zoom_Interp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Zoom")
+	float MinZoomDistance;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Zoom")
+	float MaxZoomDistance;
+
+private:
+	// Network functions
+	UFUNCTION(NetMulticast, Reliable)
+	void Multicast_SetDesiredLocation(const FVector& NewDesiredLocation);
+
+	virtual void Multicast_SetDesiredLocation_Implementation(const FVector& NewDesiredLocation);
+
+	UFUNCTION(Server, Reliable, WithValidation)
+	void Server_HandleInput(FVector MoveInput, float RotateInput, float ZoomInput);
+
+	virtual bool Server_HandleInput_Validate(FVector MoveInput, float RotateInput, float ZoomInput);
+	virtual void Server_HandleInput_Implementation(FVector MoveInput, float RotateInput, float ZoomInput);
+
+	// Process input
+	void ProcessInput(const FVector& MoveInput, const float& RotateInput, const float& ZoomInput);
+
+	// Input actions
 	void Move(const FInputActionValue& Value);
 	void Rotate(const FInputActionValue& Value);
 	void Zoom(const FInputActionValue& Value);
-
-private:
-		UPROPERTY(VisibleAnywhere, Category = Camera)
-		class USpringArmComponent* CameraBoom;
-
-		UPROPERTY(VisibleAnywhere, Category = Camera)
-		class UCameraComponent* FollowCamera;
-
-		UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
-		class UWidgetComponent* OverheadWidget;
-
-
-
-		//MOVE
-		UPROPERTY(Replicated, EditAnywhere, BlueprintReadWrite, Category = "EW Character Rotation", meta = (AllowPrivateAccess = "true"))
-		FVector DesiredLocation;
-
-		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EW Character Movement", meta = (AllowPrivateAccess = "true"))
-		float Movement_Interp;
-
-		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EW Character Movement", meta = (AllowPrivateAccess = "true"))
-		float Movement_Speed;
-
-		//ROTATION
-		UPROPERTY(Replicated, EditAnywhere, BlueprintReadWrite, Category = "EW Character Rotation", meta = (AllowPrivateAccess = "true"))
-		float DesiredYawRotation;
-
-		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EW Character Rotation", meta = (AllowPrivateAccess = "true"))
-		float Rotation_Interp;
-
-		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EW Character Rotation", meta = (AllowPrivateAccess = "true"))
-		float Rotation_Speed;
-
-		//ZOOM
-		UPROPERTY(Replicated, EditAnywhere, BlueprintReadWrite, Category = "EW Character Rotation", meta = (AllowPrivateAccess = "true"))
-		float DesiredZoom;
-
-		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EW Character Zoom", meta = (AllowPrivateAccess = "true"))
-		float ZoomSpeed;
-
-		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EW Character Zoom", meta = (AllowPrivateAccess = "true"))
-		float Zoom_Interp;
-
-		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EW Character Zoom", meta = (AllowPrivateAccess = "true"))
-		float MinZoomDistance;
-
-		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EW Character Zoom", meta = (AllowPrivateAccess = "true"))
-		float MaxZoomDistance;
-
-		void HandleInput(const FVector& MoveInput, const float& RotateInput, const float& ZoomInput);
 };
-
