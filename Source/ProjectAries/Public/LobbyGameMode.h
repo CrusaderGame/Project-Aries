50868c380f3d9cffb518eb887b51561013778d00
dam2452 <81230036+dam2452@users.noreply.github.com>
2023-10-24 13:41:41 +0200

    Sessions Join/Host

---
+++ Source/ProjectAries/Public/LobbyGameMode.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+#include "GameFramework/PlayerStart.h"
+#include "EngineUtils.h"
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "LobbyGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTARIES_API ALobbyGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	ALobbyGameMode();
+	virtual void PostLogin(APlayerController* NewPlayer) override;
+	virtual void Logout(AController* Exiting) override;
+	/*virtual APawn* SpawnDefaultPawnFor_Implementation(AController* NewPlayer, AActor* StartSpot) override;
+	virtual AActor* ChoosePlayerStart_Implementation(AController* Player) override;*/
+
+
+private:
+	bool IsPlayerStartSpotAvailable(APlayerStart* StartSpot, AController* Player);
+
+};
