776a6cdae013a0642b0062f81bf3caf40e031612
Pan Mateusz <57064828+Laavenx@users.noreply.github.com>
2024-11-05 21:07:37 +0100

    - Added grid blocker volume

--- Source/ProjectAries/ProjectAries.Build.cs
+++ Source/ProjectAries/ProjectAries.Build.cs
@@ -10,7 +10,7 @@ public class ProjectAries : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "EnhancedInput", "Niagara", "AIModule" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
